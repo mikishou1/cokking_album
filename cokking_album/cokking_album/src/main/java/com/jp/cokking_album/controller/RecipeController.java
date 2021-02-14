796ed8b81ddc56179ba2ffba53b0b4e25573280e
mikishou1 <mikishou1@gmail.com>
2021-02-14 16:38:56 +0900

    RecipeController first Commit

---
+++ cokking_album/cokking_album/src/main/java/com/jp/cokking_album/controller/RecipeController.java
@@ -0,0 +1,28 @@
+package com.jp.cokking_album.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * レシピ管理用
+ * @author msm41
+ *
+ */
+@Controller
+public class RecipeController {
+
+    /**
+     * レシピ一覧表示
+     */
+    public void showRecipes() {
+
+    }
+
+    /**
+     * レシピ登録
+     */
+    public void addRecipe() {
+
+    }
+
+}
