8419adf28da9abc0d7357b22407eea69c7980a83
mikishou1 <mikishou1@gmail.com>
2021-02-14 15:39:10 +0900

    Initial Commit

---
+++ cokking_album/cokking_album/src/main/java/com/jp/cokking_album/CokkingAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.jp.cokking_album;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokkingAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokkingAlbumApplication.class, args);
+	}
+
+}
---
+++ cokking_album/cokking_album/src/test/java/com/jp/cokking_album/CokkingAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.cokking_album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CokkingAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
