f74fbbe0030f23ed841f6480474dd7ae7ed340ba
mikishou1 <mikishou1@gmail.com>
2021-02-16 00:05:45 +0900

    jsp commit temporary

--- cokking_album/cokking_album/src/main/java/com/jp/cokking_album/controller/RecipeController.java
+++ cokking_album/cokking_album/src/main/java/com/jp/cokking_album/controller/RecipeController.java
@@ -1,6 +1,9 @@
 package com.jp.cokking_album.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -9,13 +12,18 @@ import org.springframework.stereotype.Controller;
  *
  */
 @Controller
+@RequestMapping(value="/")
 public class RecipeController {
 
     /**
      * レシピ一覧表示
+     * 初期表示として仮実装
      */
-    public void showRecipes() {
-
+    @RequestMapping(value = "/list-recipes", method = RequestMethod.GET)
+    public String showRecipes(ModelMap model) {
+        String[] list = {"カレー","ハンバーグ","牛カツ"};
+        model.put("recipes", list);
+        return "list-recipes";
     }
 
     /**
